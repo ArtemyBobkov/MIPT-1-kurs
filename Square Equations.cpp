911efbf09d6f811590b54f7082569eb0e307c282
ArtemyBobkov <70328946+ArtemyBobkov@users.noreply.github.com>
2020-08-28 15:05:56 +0300

    Квадратные уравнения v1.72

--- Square Equations.cpp
+++ Square Equations.cpp
@@ -7,7 +7,7 @@
 
 void Hello() {
     printf("## Bobkov Artemy - Square Equations \n");
-    printf("## v.1.7 \n");
+    printf("## v.1.72 \n");
     printf("## All rights are Bla Bla Bla\n");
 }
 
@@ -132,7 +132,10 @@ int SquareSols(double a, double  b, double  c, double* x1, double* x2)
                     *x2 = (-b + sqrt(D)) / (2*a);
                     return 2;
             case -1:
+                    *x1 = 0;
+                    *x2 = 0;
                     return 0;
+
             }
 
         }
@@ -194,65 +197,78 @@ int unit_testst()
     {
     int IsRight = 1;
     double x1 = 0, x2 = 0;
+
     if (SquareSols(0, 0, 0, &x1, &x2) != INF_SOLS || x1 != 0 || x2 != 0)
         {
         printf("ERROR: Failed unit_test #1 \n");
         IsRight = 0;
         }
+
     if (SquareSols(0, 0, 1, &x1, &x2) != 0 || x1 != 0 || x2 != 0)
         {
         printf("ERROR: Failed unit_test #2 \n");
         IsRight = 0;
         }
+
     if (SquareSols(0, 1, 0, &x1, &x2) != 1 || x1 != 0 || x2 != 0)
         {
         printf("ERROR: Failed unit_test #3 \n");
         IsRight = 0;
         }
+
     if (SquareSols(0, 1, 1, &x1, &x2) != 1 || x1 != -1 || x2 != 0)
         {
         printf("ERROR: Failed unit_test #4 \n");
         IsRight = 0;
         }
+
     if (SquareSols(1, 0, 0, &x1, &x2) != 1 || x1 != 0 || x2 != 0)
         {
         printf("ERROR: Failed unit_test #5 \n");
         IsRight = 0;
         }
+
     if (SquareSols(1, 0, 1, &x1, &x2) != 0 || x1 != 0 || x2 != 0)
         {
         printf("ERROR: Failed unit_test #6 \n");
         IsRight = 0;
         }
+
     if (SquareSols(1, 1, 0, &x1, &x2) != 2 || x1 != -1 || x2 != 0)
         {
         printf("ERROR: Failed unit_test #7 \n");
         IsRight = 0;
         }
-    if (SquareSols(1, 1, 1, &x1, &x2) != 0 || x1 != 0 || x2 != 0)
+
+    if (SquareSols(1, 1, 1, &x1, &x2) != 0 || x1 != 0  || x2 != 0)
         {
         printf("ERROR: Failed unit_test #8 \n");
         IsRight = 0;
         }
+
     if (SquareSols(1, 2, 1, &x1, &x2) != 1 || x1 != -1 || x2 != -1)
         {
         printf("ERROR: Failed unit_test #9 \n");
         IsRight = 0;
         }
+
     if (SquareSols(1, 0, -1, &x1, &x2) != 2 || x1 != -1 || x2 != 1)
         {
         printf("ERROR: Failed unit_test #10 \n");
         IsRight = 0;
         }
+
     if (SquareSols(2, 5, 2, &x1, &x2) != 2 || x1 != -2 || x2 != -0.5)
         {
         printf("ERROR: Failed unit_test #11 \n");
         IsRight = 0;
         }
+
     if (IsRight)
         {
         return 0;
         }
+
     else
         {
         return 1;
@@ -265,6 +281,7 @@ main ()
     printf("Enter equation coefficients: \n");
     double a = 0, b = 0, c = 0;
     int read_good = 0;
+
     do
         {
         fflush(stdin);
